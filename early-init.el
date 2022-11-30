37aa1e7ac7e9fab7d58edd5b527a67ea221215fd
Bret Horne <bret.horne@noaa.gov>
2022-11-30 16:24:30 -0600

    Prep for new layout

--- early-init.el
+++ early-init.el
@@ -1,6 +1,12 @@
 ;;-*- lexical-binding: t -*-
 
+;; Prevent compilation from popping up warnings
+(setq native-comp-async-report-warnings-errors nil)
+
+;; Main hook to run after core loads
 (add-hook 'emacs-startup-hook
+	  ;; restore the disabled checking against file handler alist
+	  ;; and set a smaller gc threshold to gc more but have shorter gc's as well
 	  (let ((restore-list file-name-handler-alist)
 		(threshold 8388608)) ; 8mb
 	    (lambda ()
@@ -12,13 +18,22 @@
 	      (setq file-name-handler-alist restore-list
 		    gc-cons-threshold threshold)
 	      (garbage-collect)))
-	  t)
+	  ;; wait later to restore in case others are using `emacs-startup-hook'
+	  100)
 
-(setq gc-cons-threshold 100000000;;lets try 100mb;;most-positive-fixnum
+;; Some simple performance tweaks
+;; 
+;;;; First let's set the gc threshold to 100 mb so we gc less during startup.
+;;;; `most-positive-fixnum' is another option if there is more demand during startup
+(setq gc-cons-threshold 100000000
+      ;; always load new bytecode
       load-prefer-newer noninteractive
+      ;; dont load packages until we're ready
       package-enable-at-startup nil
+      ;; disable checking against file handler alist when using file primitives for startup
       file-name-handler-alist nil)
 
+;; We wont be needing these where we're going 🛸
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
